e83da1a4f7acdc89216924b5145f23b2aabf1830
rahulroynipon <rahulroynipon@gmail.com>
2024-06-16 03:48:42 +0600

    complete the team section

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -1,9 +1,14 @@
+import Button from "../reuseables/Button";
 import HeaderTitle from "../reuseables/HeaderTitle";
 import TeamCard from "../reuseables/TeamCard";
 
+import member1 from "./../assets/img/team1.png";
+import member2 from "./../assets/img/team2.png";
+import member3 from "./../assets/img/team3.png";
+
 export default function TeamSection() {
   return (
-    <main className="width padding-x padding-y mt-16">
+    <main className="width padding-x padding-y mt-16 flex flex-col gap-7">
       <HeaderTitle
         title={"Team"}
         content={
@@ -11,14 +16,63 @@ export default function TeamSection() {
         }
       />
 
-      <section className="mt-8 md:mt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-7">
-        <TeamCard />
-        <TeamCard />
-        <TeamCard />
-        <TeamCard />
-        <TeamCard />
-        <TeamCard />
+      <section className="md:mt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-7">
+        {TeamDetails?.map((item) => (
+          <TeamCard key={item.id} data={item} />
+        ))}
       </section>
+      <Button btnText="See all team" clName={"md:self-end"} />
     </main>
   );
 }
+
+export const TeamDetails = [
+  {
+    id: "1",
+    name: "John Smith",
+    position: "CEO and Founder",
+    img: member1,
+    content:
+      "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    position: "Director of Operations",
+    img: member2,
+    content:
+      "7+ years of experience in project management and team leadership. Strong organizational and communication skills",
+  },
+  {
+    id: "3",
+    name: "Michael Brown",
+    position: "Senior SEO Specialist",
+    img: member3,
+    content:
+      "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
+  },
+  {
+    id: "4",
+    name: "Emily Johnson",
+    position: "PPC Manager",
+    img: member3,
+    content:
+      "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
+  },
+  {
+    id: "5",
+    name: "Brian Williams",
+    position: "Social Media Specialist",
+    img: member2,
+    content:
+      "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement",
+  },
+  {
+    id: "6",
+    name: "Sarah Kim",
+    position: "Content Creator",
+    img: member1,
+    content:
+      "2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries",
+  },
+];
--- src/reuseables/TeamCard.jsx
+++ src/reuseables/TeamCard.jsx
@@ -1,25 +1,21 @@
-import img from "./../assets/img/team1.png";
 import icon from "./../assets/icon/linkden.svg";
 
-export default function TeamCard() {
+export default function TeamCard({ data }) {
   return (
     <main className="border border-b-4 border-pBlack rounded-2xl p-5 md:p-10 flex flex-col gap-6">
-      <section className="flex justify-between items-end">
-        <img src={img} alt="" />
-        <h4 className="flex flex-col font-space-grotesk">
-          <span className="text-2xl">John Smith</span>
-          <span>CEO and Founder</span>
+      <section className="flex justify-between items-center gap-4">
+        <img className="mn-h-16 h-20 max-h-22" src={data?.img} alt="profile" />
+        <h4 className="flex flex-col font-space-grotesk self-end">
+          <span className="text-2xl">{data?.name}</span>
+          <span>{data?.position}</span>
         </h4>
         <div className="border rounded-full self-start p-2 bg-pBlack">
-          <img src={icon} alt="" />
+          <img src={icon} alt="linkden" />
         </div>
       </section>
 
       <div className="h-[1.5px] bg-pBlack/50"></div>
-      <section>
-        10+ years of experience in digital marketing. Expertise in SEO, PPC, and
-        content strategy
-      </section>
+      <section>{data?.content}</section>
     </main>
   );
 }
